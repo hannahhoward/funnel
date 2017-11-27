d4d6a75ba2c0d6b8a82d878af53fe850a82d9cb7
Evelyn Masso <evelyn@carbonfive.com>
2017-11-27 13:00:06 -0800

    generate git strategy schema

---
+++ lib/funnel/git/strategy.ex
@@ -0,0 +1,21 @@
+defmodule Funnel.Git.Strategy do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Funnel.Git.Strategy
+
+
+  schema "strategies" do
+    field :description, :string
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Strategy{} = strategy, attrs) do
+    strategy
+    |> cast(attrs, [:name, :description])
+    |> unique_constraint(:name)
+    |> validate_required([:name])
+  end
+end
